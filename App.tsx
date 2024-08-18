605437c4ee6dc5856c0a16c425445819e1e768fb
kdh30806 <kdh30806@naver.com>
2024-08-18 22:09:53 +0900

    init

--- App.tsx
+++ App.tsx
@@ -1,118 +1,141 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {TouchableOpacity, View, StyleSheet, Text} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import styled from 'styled-components/native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Value = styled.Text`
+  color: red;
+  font-size: 100px;
+  margin-left: 30px;
+  margin-top: 15px;
+`;
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Button = ({text, icon: IconComponent, iconName, onPress}) => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: 'lightgrey',
+      width: 75,
+      borderRadius: 5,
+      margin: 20,
+    }}
+    onPress={onPress}>
+    {text ? (
+      <Text style={{fontSize: 40, textAlign: 'center'}}>{text}</Text>
+    ) : (
+      <IconComponent name={iconName} size={50} color="black" />
+    )}
+  </TouchableOpacity>
+);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const calculate = (op, num1, num2) => {
+  switch (op) {
+    case 'plus':
+      return num1 + num2;
+    case 'minus':
+      return num1 - num2;
+    case 'times':
+      return num1 * num2;
+    case 'divide':
+      return num1 / num2;
+    default:
+      console.error(`Invalid operation ${op}`);
+      return num2;
+  }
+};
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+export default function App() {
+  const [result, setResult] = useState(null);
+  const [input, setInput] = useState('');
+  const [operation, setOperation] = useState(null);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleChangeInput = num => {
+    setInput(input + num);
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleOperationPress = op => {
+    if (input === '') return;
+    if (result === null) {
+      setResult(parseFloat(input));
+    } else {
+      setResult(calculate(operation, result, parseFloat(input)));
+    }
+    setInput('');
+    setOperation(op);
+  };
+
+  const handleEqualPress = () => {
+    if (operation && input) {
+      setResult(calculate(operation, result, parseFloat(input)));
+      setInput('');
+      setOperation(null);
+    }
+  };
+
+  const handleResetPress = () => {
+    setResult(null);
+    setInput('');
+    setOperation(null);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <View>
+        <Value>{input || result || 0}</Value>
+      </View>
+      <View style={styles.container}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(number => (
+          <Button
+            key={number}
+            text={number}
+            onPress={() => handleChangeInput(number.toString())}
+          />
+        ))}
+        <Button
+          icon={AntDesign}
+          iconName={'plus'}
+          onPress={() => handleOperationPress('plus')}
+        />
+        <Button
+          icon={AntDesign}
+          iconName={'minus'}
+          onPress={() => handleOperationPress('minus')}
+        />
+        <Button
+          icon={FontAwesome6}
+          iconName={'equals'}
+          onPress={handleEqualPress}
+        />
+        <Button
+          icon={Feather}
+          iconName={'x'}
+          onPress={() => handleOperationPress('times')}
+        />
+        <Button
+          icon={FontAwesome6}
+          iconName={'divide'}
+          onPress={() => handleOperationPress('divide')}
+        />
+        <Button
+          icon={MaterialIcons}
+          iconName={'restart-alt'}
+          onPress={handleResetPress}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: '60%',
   },
 });
-
-export default App;
